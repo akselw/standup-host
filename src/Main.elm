ce1a9ad5d07e94cce68d560fdc136a94638bc431
Aksel Wester <aksel.wester@eika.no>
2021-03-23 09:10:26 +0100

    Legg til animasjon på neste arbeidsdag-side

--- src/Main.elm
+++ src/Main.elm
@@ -3,8 +3,9 @@ module Main exposing (main)
 import Browser
 import Dato exposing (Dato)
 import Html exposing (..)
-import Html.Attributes exposing (class, type_)
+import Html.Attributes exposing (class, classList, type_)
 import Html.Events exposing (onClick)
+import Process
 import Random
 import Random.List
 import Task
@@ -15,12 +16,22 @@ import Time exposing (Month(..), Posix)
 --- MODEL ---
 
 
+type AnimationState
+    = IkkeVisNoe
+    | VisFørsteSetning
+    | VisAndreSetning
+    | VisStandupVert
+    | VisKanIkkeTekst
+    | VisAlt
+
+
 type Model
     = Init
     | Success
         { dagensDato : Dato
         , dagensRekkefølge : List String
         , morgensdagensRekkefølge : List String
+        , morgendagensAnimationState : AnimationState
         , valgtDag : ValgtDag
         }
 
@@ -50,7 +61,9 @@ teamBruke =
 type Msg
     = TimeReceived Posix
     | VelgNyPersonIDag
+    | VelgNyPersonNesteArbeidsdag
     | EndreFane ValgtDag
+    | AnimationTick
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -75,6 +88,7 @@ update msg model =
                         |> Random.step (Random.List.shuffle teamBruke)
                         |> Tuple.first
                 , valgtDag = Idag
+                , morgendagensAnimationState = IkkeVisNoe
                 }
             , Cmd.none
             )
@@ -92,13 +106,67 @@ update msg model =
                         [] ->
                             ( model, Cmd.none )
 
+        VelgNyPersonNesteArbeidsdag ->
+            case model of
+                Init ->
+                    ( model, Cmd.none )
+
+                Success record ->
+                    case record.morgensdagensRekkefølge of
+                        _ :: rest ->
+                            ( Success { record | morgensdagensRekkefølge = rest }, Cmd.none )
+
+                        [] ->
+                            ( model, Cmd.none )
+
         EndreFane valgtDag ->
             case model of
                 Init ->
                     ( model, Cmd.none )
 
                 Success modelInfo ->
-                    ( Success { modelInfo | valgtDag = valgtDag }, Cmd.none )
+                    ( Success { modelInfo | valgtDag = valgtDag }
+                    , Process.sleep 500
+                        |> Task.perform (\_ -> AnimationTick)
+                    )
+
+        AnimationTick ->
+            case model of
+                Init ->
+                    ( model, Cmd.none )
+
+                Success modelInfo ->
+                    ( Success { modelInfo | morgendagensAnimationState = nesteAnimationState modelInfo.morgendagensAnimationState }
+                    , case nesteAnimationState modelInfo.morgendagensAnimationState of
+                        VisAlt ->
+                            Cmd.none
+
+                        _ ->
+                            Process.sleep 1000
+                                |> Task.perform (\_ -> AnimationTick)
+                    )
+
+
+nesteAnimationState : AnimationState -> AnimationState
+nesteAnimationState animationState =
+    case animationState of
+        IkkeVisNoe ->
+            VisFørsteSetning
+
+        VisFørsteSetning ->
+            VisAndreSetning
+
+        VisAndreSetning ->
+            VisStandupVert
+
+        VisStandupVert ->
+            VisKanIkkeTekst
+
+        VisKanIkkeTekst ->
+            VisAlt
+
+        VisAlt ->
+            VisAlt
 
 
 
@@ -107,39 +175,125 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    div [ class "wrapper" ]
+        [ div [ class "standup-host" ]
+            (viewContent model)
+        ]
+
+
+viewContent : Model -> List (Html Msg)
+viewContent model =
     case model of
         Init ->
-            text ""
+            [ text "" ]
 
-        Success { dagensDato, dagensRekkefølge } ->
-            case List.head dagensRekkefølge of
-                Just standupVert ->
-                    div [ class "wrapper" ]
-                        [ div [ class "standup-host" ]
-                            [ div []
-                                [ button [ onClick (EndreFane Idag), type_ "button" ] [ text "I dag" ]
-                                , button [ onClick (EndreFane NesteArbeidsdag), type_ "button" ]
-                                    [ dagensDato
-                                        |> Dato.nesteArbeidsdag
-                                        |> Dato.toUkedagString
-                                        |> text
-                                    ]
-                                ]
-                            , h1 [] [ text standupVert ]
-                            , p [] [ text "skal holde standup" ]
-                            , p [] [ text (Dato.toString dagensDato) ]
-                            , button
-                                [ onClick VelgNyPersonIDag ]
-                                [ text (standupVert ++ " kan ikke") ]
-                            ]
-                        ]
+        Success { dagensDato, dagensRekkefølge, morgensdagensRekkefølge, valgtDag, morgendagensAnimationState } ->
+            [ div []
+                [ button [ onClick (EndreFane Idag), type_ "button" ] [ text "I dag" ]
+                , button [ onClick (EndreFane NesteArbeidsdag), type_ "button" ]
+                    [ dagensDato
+                        |> Dato.nesteArbeidsdag
+                        |> Dato.toUkedagString
+                        |> text
+                    ]
+                ]
+            , case valgtDag of
+                Idag ->
+                    viewIdag dagensRekkefølge dagensDato
 
-                Nothing ->
-                    div [ class "wrapper" ]
-                        [ div [ class "standup-host" ]
-                            [ text "Da kunne visst ingen da..."
-                            ]
+                NesteArbeidsdag ->
+                    viewNesteVirkedag morgendagensAnimationState morgensdagensRekkefølge dagensDato
+            ]
+
+
+viewIdag : List String -> Dato -> Html Msg
+viewIdag rekkefølge dagensDato =
+    case List.head rekkefølge of
+        Just standupVert ->
+            div [ class "wrapper" ]
+                [ div [ class "standup-host" ]
+                    [ h1 [] [ text standupVert ]
+                    , p [] [ text ("skal holde standup i dag, " ++ String.toLower (Dato.toUkedagString dagensDato) ++ " " ++ Dato.toString dagensDato ++ ".") ]
+                    , button
+                        [ onClick VelgNyPersonIDag ]
+                        [ text (standupVert ++ " kan ikke") ]
+                    ]
+                ]
+
+        Nothing ->
+            text "Da kunne visst ingen da..."
+
+
+type AnimationElememt
+    = FørsteSetning
+    | AndreSetning
+    | StandupVert
+    | KanIkkeTekst
+    | NesteKnapp
+
+
+elementSkalVises : AnimationState -> AnimationElememt -> Bool
+elementSkalVises animationState element =
+    case ( animationState, element ) of
+        ( IkkeVisNoe, _ ) ->
+            False
+
+        ( VisFørsteSetning, FørsteSetning ) ->
+            True
+
+        ( VisAndreSetning, FørsteSetning ) ->
+            True
+
+        ( VisAndreSetning, AndreSetning ) ->
+            True
+
+        ( VisStandupVert, FørsteSetning ) ->
+            True
+
+        ( VisStandupVert, AndreSetning ) ->
+            True
+
+        ( VisStandupVert, StandupVert ) ->
+            True
+
+        ( VisKanIkkeTekst, _ ) ->
+            True
+
+        ( VisAlt, _ ) ->
+            True
+
+        _ ->
+            False
+
+
+animationClasses : AnimationState -> AnimationElememt -> Html.Attribute msg
+animationClasses animationState element =
+    classList [ ( "fade-in", True ), ( "fade-in-hidden", not (elementSkalVises animationState element) ) ]
+
+
+viewNesteVirkedag : AnimationState -> List String -> Dato -> Html Msg
+viewNesteVirkedag animationState nesteVirkedagsRekkefølge dagensDato =
+    case List.head nesteVirkedagsRekkefølge of
+        Just standupVert ->
+            div [ class "wrapper" ]
+                [ div [ class "standup-host" ]
+                    [ p [ animationClasses animationState FørsteSetning ]
+                        [ text ("Den som skal holde standup på " ++ String.toLower (Dato.toUkedagString (Dato.nesteArbeidsdag dagensDato)))
                         ]
+                    , p [ animationClasses animationState AndreSetning ]
+                        [ text "er" ]
+                    , h1 [ animationClasses animationState StandupVert ]
+                        [ text standupVert ]
+                    , button
+                        [ animationClasses animationState NesteKnapp
+                        , onClick VelgNyPersonNesteArbeidsdag
+                        ]
+                        [ text (standupVert ++ " kan ikke") ]
+                    ]
+                ]
+
+        Nothing ->
+            text "Da kunne visst ingen da..."
 
 
 
