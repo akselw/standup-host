e35d7ff33b1538392333a440b1f90345ab09c153
Aksel Wester <aksel@wester.co>
2023-07-26 14:16:43 +0200

    Legg til prettier

--- src/interop.js
+++ src/interop.js
@@ -1,12 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"
 
 // This is called BEFORE your Elm app starts up
 //
 // The value returned here will be passed as flags
 // into your `Shared.init` function.
 export const flags = ({ env }) => {
-    return { apiKey: env.SUPABASE_API_KEY };
-};
+    return { apiKey: env.SUPABASE_API_KEY }
+}
 
 // This is called AFTER your Elm app starts up
 //
@@ -14,25 +14,27 @@ export const flags = ({ env }) => {
 // to your Elm application, or subscribe to incoming
 // messages from Elm
 export const onReady = async ({ app, env }) => {
-    const supabaseUrl = "https://xluvzigagcthclpwrzhj.supabase.co";
-    const supabaseKey = env.SUPABASE_API_KEY;
-    const supabase = createClient(supabaseUrl, supabaseKey);
+    const supabaseUrl = "https://xluvzigagcthclpwrzhj.supabase.co"
+    const supabaseKey = env.SUPABASE_API_KEY
+    const supabase = createClient(supabaseUrl, supabaseKey)
 
     app.ports.authentication.subscribe(async (action) => {
         switch (action.type) {
             case "LOGIN":
                 let { data, error } = await supabase.auth.signInWithOAuth({
                     provider: "github",
-                    options: { redirectTo: "http://localhost:1234/oauth" },
-                });
-                return;
+                    options: {
+                        redirectTo: "http://localhost:1234/oauth",
+                    },
+                })
+                return
             default:
-                return;
+                return
         }
-    });
+    })
     // let { data, error } = await supabase.auth.signInWithOAuth({
     //     provider: 'github'
     //     , options: { redirectTo: 'http://localhost:1234/min-side'}
     // })
     // console.log({ data , error });
-};
+}
