1334710b6a2977910fcc9d2d6cf3ed1e816bacff
Aksel Wester <aksel@wester.co>
2023-08-07 23:24:16 +0200

    Legg til at /login sjekker om bruker har token før redirect

--- src/Pages/Login.elm
+++ src/Pages/Login.elm
@@ -7,13 +7,14 @@ import Page exposing (Page)
 import Route exposing (Route)
 import Route.Path
 import Shared
+import Shared.Model exposing (AccessTokenStatus(..))
 import View exposing (View)
 
 
 page : Shared.Model -> Route () -> Page Model Msg
 page shared route =
     Page.new
-        { init = init route
+        { init = init shared route
         , update = update
         , subscriptions = subscriptions
         , view = view
@@ -28,15 +29,30 @@ type alias Model =
     {}
 
 
-init : Route () -> () -> ( Model, Effect Msg )
-init route () =
+init : Shared.Model -> Route () -> () -> ( Model, Effect Msg )
+init shared route () =
     ( {}
-    , route.query
-        |> Dict.get Authentication.redirectKey
-        |> Maybe.andThen Route.Path.fromString
-        |> Maybe.map Authentication.loginWithRedirectUrl
-        |> Maybe.withDefault Authentication.login
-        |> Effect.sendCmd
+    , case shared.accessToken of
+        Token _ ->
+            route.query
+                |> Dict.get Authentication.redirectKey
+                |> Maybe.andThen Route.Path.fromString
+                |> Maybe.withDefault Route.Path.MinSide
+                |> (\redirectPath ->
+                        Effect.pushRoute
+                            { path = redirectPath
+                            , query = Dict.empty
+                            , hash = Nothing
+                            }
+                   )
+
+        _ ->
+            route.query
+                |> Dict.get Authentication.redirectKey
+                |> Maybe.andThen Route.Path.fromString
+                |> Maybe.map Authentication.loginWithRedirectUrl
+                |> Maybe.withDefault Authentication.login
+                |> Effect.sendCmd
     )
 
 
